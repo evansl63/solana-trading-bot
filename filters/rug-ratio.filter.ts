8a386dd52b441740414a0d73a518443d193a83f2
sagle4 <EvansL63@cardiff.ac.uk>
2024-04-30 15:09:47 +0100

    Update rug_ratio API check

--- filters/rug-ratio.filter.ts
+++ filters/rug-ratio.filter.ts
@@ -9,8 +9,8 @@ export class RugRatioFilter implements Filter {
       const response = await axios.get(`https://gmgn.ai/defi/quotation/v1/tokens/sol/${poolKeys.baseMint}`);
       const data = response.data;
 
-      if (data.code === 0 && data.data && data.data.token && data.data.token.rug_ratio === 1) {
-        return { ok: false, message: 'RugRatio -> Token has a rug_ratio of 1' };
+      if (data.code === 0 && data.data && data.data.token && data.data.token.rug_ratio > 0) {
+        return { ok: false, message: 'RugRatio -> Token has a rug_ratio greater than 0' };
       }
 
       return { ok: true };
